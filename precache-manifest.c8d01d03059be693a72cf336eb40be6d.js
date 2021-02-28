d6875700c1ae8c7e5297bc07822c22be12ea888f
DevLikeMike <michael@MacBook-Pro.local>
2021-02-28 14:05:11 -0500

    Updates

---
+++ precache-manifest.c8d01d03059be693a72cf336eb40be6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11697da392391e54b7de998e84e460c",
+    "url": "/fed-challenge-starter/index.html"
+  },
+  {
+    "revision": "0ce6ffbbda1887e60fcb",
+    "url": "/fed-challenge-starter/static/css/main.07239095.chunk.css"
+  },
+  {
+    "revision": "743c642d1dd08585ebc3",
+    "url": "/fed-challenge-starter/static/js/2.5e62a50e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/fed-challenge-starter/static/js/2.5e62a50e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ce6ffbbda1887e60fcb",
+    "url": "/fed-challenge-starter/static/js/main.3e0319c4.chunk.js"
+  },
+  {
+    "revision": "ddf4e5f1c600b5f0db56",
+    "url": "/fed-challenge-starter/static/js/runtime-main.3972a04a.js"
+  }
+]);
